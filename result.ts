dfdb92c90517590a515de9de15876aba02b96262
Eyal Shalev <eyalsh@gmail.com>
2023-08-03 19:19:27 +0300

    deno fmt

--- result.ts
+++ result.ts
@@ -1,15 +1,15 @@
-import {isPromiseLike} from "./promise.ts";
-import {ensureError} from "./error.ts";
+import { isPromiseLike } from "./promise.ts";
+import { ensureError } from "./error.ts";
 
 export type RValue<T> = [T, undefined];
 export type RError<E extends Error = Error> = [undefined, E];
 export type Result<T, E extends Error = Error> = RValue<T> | RError<E>;
 
 export function resultify<TArgs extends unknown[], TReturn>(
-		fn: (...args: TArgs) => TReturn,
+  fn: (...args: TArgs) => TReturn,
 ): (...args: TArgs) => Result<Awaited<TReturn>>;
 export function resultify<TArgs extends unknown[], TReturn>(
-		fn: (...args: TArgs) => PromiseLike<TReturn>,
+  fn: (...args: TArgs) => PromiseLike<TReturn>,
 ): (...args: TArgs) => PromiseLike<Result<Awaited<TReturn>>>;
 
 /**
@@ -17,20 +17,20 @@ export function resultify<TArgs extends unknown[], TReturn>(
  * If the function returns a promise-like, the result tuple will be wrapped in a promise-like as well..
  */
 export function resultify<TArgs extends unknown[], TReturn>(
-		fn: (...args: TArgs) => TReturn | PromiseLike<TReturn>,
+  fn: (...args: TArgs) => TReturn | PromiseLike<TReturn>,
 ) {
-	return (...args: TArgs) => {
-		try {
-			const value = fn(...args);
-			if (isPromiseLike(value)) {
-				return value.then(
-						(value) => [value, undefined],
-						(error) => [undefined, ensureError(error)],
-				);
-			}
-			return [value, undefined];
-		} catch (e) {
-			return [undefined, ensureError(e)];
-		}
-	};
+  return (...args: TArgs) => {
+    try {
+      const value = fn(...args);
+      if (isPromiseLike(value)) {
+        return value.then(
+          (value) => [value, undefined],
+          (error) => [undefined, ensureError(error)],
+        );
+      }
+      return [value, undefined];
+    } catch (e) {
+      return [undefined, ensureError(e)];
+    }
+  };
 }
