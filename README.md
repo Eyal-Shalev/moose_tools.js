b9144496693ccaf64ece9047aac641ce13599034
Eyal Shalev <eyalsh@gmail.com>
2023-08-09 21:58:26 +0300

    Compile script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Moose Tools
+
+> A zero-dependency collection of JavaScript functions
+
+[![Test](https://github.com/Eyal-Shalev/moose_tools.js/actions/workflows/test.yaml/badge.svg)](https://github.com/Eyal-Shalev/moose_tools.js/actions/workflows/test.yaml)
---
+++ scripts/compile.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run --unstable --allow-net
+
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt@0.38.0/mod.ts";
+
+await emptyDir("./dist");
+
+await build({
+	entryPoints: ["./mod.ts"],
+	outDir: "./dist",
+	compilerOptions: {
+		target: "ES2022",
+	},
+	typeCheck: "both",
+	shims: {
+		// see JS docs for overview and more options
+		deno: true,
+	},
+	package: {
+		// package.json properties
+		name: "moose_tools",
+		version: Deno.args[0],
+		description: "Moose Tools",
+		license: "Mozilla Public License 2.0",
+		repository: {
+			type: "git",
+			url: "git+https://github.com/Eyal-Shalev/moose_tools.js.git",
+		},
+		bugs: {
+			url: "https://github.com/Eyal-Shalev/moose_tools.js/issues",
+		},
+		"engines": {
+			"node": ">=16.11.0",
+		},
+	},
+	postBuild() {
+		// steps to run after building and before running the tests
+		Deno.copyFileSync("LICENSE", "dist/LICENSE");
+		Deno.copyFileSync("README.md", "dist/README.md");
+	},
+});
