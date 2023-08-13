1489d61d920f104b77f5d3c0f61618acb7d2a16a
Eyal Shalev <eyalsh@gmail.com>
2023-08-14 02:22:51 +0300

    iterable: Add memo, take & takeLast

--- iteration.ts
+++ iteration.ts
@@ -1,34 +1,113 @@
-export type TypedIterable<T, TReturn = void, TNext = never> = Iterable<T> & {
+export type TypedIterable<T, TReturn = void, TNext = undefined> = {
 	[Symbol.iterator](): Iterator<T, TReturn, TNext>;
 };
+
+export type TypedIterableIterator<T, TReturn = void, TNext = undefined> =
+	& Iterator<T, TReturn, TNext>
+	& TypedIterable<T, TReturn, TNext>;
+
+export type BetterGenerator<T, TReturn = void, TNext = undefined> = Generator<
+	T,
+	TReturn,
+	TNext
+>;
+
+export type IterableOrIterator<T, TReturn = void, TNext = undefined> =
+	| TypedIterable<T, TReturn, TNext>
+	| Iterator<T, TReturn, TNext>;
 
 /**
  * Returns an iterable of numbers from start (inclusive) to end (exclusive) by step.
  */
-export function range(
+export function* range(
 	start = 0,
 	end = Infinity,
 	step = 1,
-): TypedIterable<number> {
+): BetterGenerator<number> {
+	for (let i = start; i < end; i += step) {
+		yield i;
+	}
+}
+
+/**
+ * Returns an iterable of numbers from start (inclusive) to end (exclusive) by step.
+ */
+export function* repeat<T>(value: T, times = Infinity): BetterGenerator<T> {
+	if (times < 0) throw new RangeError(`times must be >= 0`);
+	for (let i = 0; i < times; i++) {
+		yield value;
+	}
+}
+
+export function memo<T, TNext = undefined>(
+	iterableOrIterator: IterableOrIterator<T, void, TNext>,
+): TypedIterable<T, void, TNext> {
+	const cache: T[] = [];
+	const iterator = iteratorFromIterable(iterableOrIterator);
+	let nextParams: [] | [TNext] = [];
 	return {
-		*[Symbol.iterator](): Iterator<number, void, never> {
-			for (let i = start; i < end; i += step) {
-				yield i;
+		*[Symbol.iterator](): Iterator<T, void, TNext> {
+			for (const value of cache) {
+				yield value;
+			}
+			let result = iterator.next(...nextParams);
+			while (!result.done) {
+				cache.push(result.value);
+				const nextParam = yield result.value;
+				nextParams = nextParam === undefined ? [] : [nextParam];
+				result = iterator.next(...nextParams);
 			}
 		},
 	};
 }
 
-
-/**
- * Returns an iterable of numbers from start (inclusive) to end (exclusive) by step.
- */
-export function repeat<T>(value: T, times = Infinity): TypedIterable<T> {
+export function take<T, TReturn = void, TNext = unknown>(
+	iterableOrIterator: IterableOrIterator<T, TReturn, TNext>,
+	count = 1,
+): TypedIterable<T, TReturn | void, TNext> {
+	if (count < 0) throw new RangeError(`count must be >= 0`);
+	const iterator = iteratorFromIterable(iterableOrIterator);
 	return {
-		*[Symbol.iterator](): Iterator<T, void, never> {
-			for (let i = 0; i < times; i++) {
-				yield value;
+		*[Symbol.iterator](): Iterator<T, TReturn | void, TNext> {
+			let nextParam: TNext | undefined = undefined;
+			for (let i = 0; i < count; i++) {
+				const nextParams: [] | [TNext] = nextParam === undefined
+					? []
+					: [nextParam];
+				const result = iterator.next(...nextParams);
+				if (!result.done) {
+					nextParam = yield result.value;
+				} else {
+					return result.value;
+				}
 			}
 		},
 	};
+}
+
+export function* takeLast<T>(
+	list: ArrayLike<T>,
+	count = 1,
+): BetterGenerator<T> {
+	if (count < 0) throw new RangeError(`count must be >= 0`);
+	for (let i = list.length - 1; i >= 0 && i >= count; i--) {
+		yield list[i];
+	}
+}
+
+export function isIterable(obj: unknown): obj is TypedIterable<unknown> {
+	return typeof obj === "object" && !!obj && Symbol.iterator in obj &&
+		typeof obj[Symbol.iterator] === "function";
+}
+
+/**
+ * Returns an iterator from an iterable or iterator.
+ */
+function iteratorFromIterable<T, TReturn = void, TNext = undefined>(
+	iter: TypedIterable<T, TReturn, TNext> | Iterator<T, TReturn, TNext>,
+): Iterator<T, TReturn, TNext> {
+	if (Symbol.iterator in iter) {
+		return iter[Symbol.iterator]();
+	}
+	return iter;
 }
--- iteration_test.ts
+++ iteration_test.ts
@@ -1,4 +1,4 @@
-import { range, repeat } from "./iteration.ts";
+import { memo, range, repeat, take } from "./iteration.ts";
 import { assertEquals } from "https://deno.land/std@0.195.0/assert/assert_equals.ts";
 import { describe, it } from "https://deno.land/std@0.195.0/testing/bdd.ts";
 
@@ -9,13 +9,27 @@ describe("iteration", function () {
 			const result = [...iterator];
 			assertEquals(result, [-3, -1.5, 0, 1.5]);
 		});
+		it("", function () {
+			const iterator = range();
+			assertEquals(Array.from(take(iterator, 3)), [0, 1, 2]);
+			assertEquals(Array.from(take(iterator, 3)), [3, 4, 5]);
+		});
 	});
 
 	describe("repeat", function () {
 		it("should return an iterable of the same value repeated", function () {
 			const foo = Symbol("foo");
 			const iterator = repeat(foo, 3);
-			assertEquals([...iterator], [foo, foo, foo])
+			assertEquals([...iterator], [foo, foo, foo]);
+		});
+	});
+
+	describe("memo", function () {
+		it("should return an iterable that caches the values of the original iterable", function () {
+			const memoed = memo(range());
+			assertEquals([...take(memoed, 3)], [0, 1, 2]);
+			assertEquals([...take(memoed, 3)], [0, 1, 2]);
+			assertEquals([...take(memoed, 3)], [0, 1, 2]);
 		});
 	});
 });
